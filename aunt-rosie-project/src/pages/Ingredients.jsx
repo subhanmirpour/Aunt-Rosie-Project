6755571b3cb115d381685d2f039b4c9fa6ff4208
AaronDimac <aaron.dimaculangan5@gmail.com>
2025-03-25 20:46:56 -0400

    fixed pagination of ingredients list

--- aunt-rosie-project/src/pages/Ingredients.jsx
+++ aunt-rosie-project/src/pages/Ingredients.jsx
@@ -37,7 +37,7 @@ const Ingredients = () => {
   const loadIngredients = async () => {
     try {
       setIsLoading(true);
-      const { data, count } = await fetchIngredients(page, ITEMS_PER_PAGE);
+      const { data, count } = await fetchIngredients({ page, itemsPerPage: ITEMS_PER_PAGE });
       setIngredients(data);
       setTotalCount(count);
     } catch (error) {
